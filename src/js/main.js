ba49d66a3accc96c65ef06b39f6b21779a759fc2
sydorova-nataliya <nsidorova200363@gmail.com>
2022-10-26 23:17:53 +0300

    added API_KEY

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 export const {API_KEY} = process.env;
+const getPopular = '/movie/popular';
+const getTopRated= '/movie/top_rated';
+const getUpcoming= '/movie/upcoming';
+
+function fetchURL(get, API_KEY){
+    const http = 'https://api.themoviedb.org/3';
+    return `${http}${get}?api_key=${API_KEY}`
+}
+
+fetch(fetchURL(getPopular,API_KEY))
+.then(res=>res.json())
+.then(data=>{
+    console.log(data);
+})
