5d84177c291b7e3e2dd127283eaedafd316e544a
sydorova-nataliya <nsidorova200363@gmail.com>
2022-11-01 15:41:53 +0300

    added constants in scss

--- src/js/templates.js
+++ src/js/templates.js
@@ -4,7 +4,7 @@ export const renderMovies = (movies, title) => (`
   <div class="container">
     <section>
       <h2 class="name-part">${title}</h2>
-      <div class="section popular__section">
+      <div class="section">
         ${movies.map(movie => renderMovie(movie)).join('')}
       </div>
     </section>
@@ -14,7 +14,7 @@ export const renderMovies = (movies, title) => (`
 export const renderMovie = ({id, title, poster_path,original_title, vote_average, release_date}) => (`
         <div class="movie">
             <a href="#movieId=${id}" >
-                <img class="movie__photo photo " src="${generateImageUrl(poster_path)}" alt="${title}" loading="lazy">
+                <img class="movie__photo" src="${generateImageUrl(poster_path)}" alt="${title}" loading="lazy">
             </a>
             <a class="movie__title">${original_title}</a>
             <span class="movie__popularity">${calculatePopularity(vote_average)}</span>
